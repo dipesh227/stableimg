287c518ed801bc9f9fdc9a6065a2697aac3aae41
dipesh227 <dipeshjoshi227@gmail.com>
2024-09-29 14:15:19 +0530

    home page done

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  image: {
+    domains: ["./src./assets/images"],
+    loader: "imgix",
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "stableimg.vercel.app",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
---
+++ src/components/header.tsx
@@ -0,0 +1,45 @@
+import { FC } from 'react';
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+import { ModeToggle } from './ModeToggle';
+
+type HeaderProps = object
+const Header: FC<HeaderProps> = () => {
+    return (
+        <header className="shadow-2xl">
+            <div className="container mx-auto px-10 py-1 flex justify-between items-center border-b-2">
+                <div className="flex flex-col items-center">
+                    <Link href="/" className=" gradient-text text-4xl font-bold transition-all duration-300 hover:scale-105">
+                        Stabel Image
+                    </Link>
+                    <span className="text-sm text-gray-500">Free AI Image Generator</span>
+                </div>
+                <nav>
+                    <ul className="flex space-x-6">
+                        <li>
+                            <Link href="/" className="transition-all duration-300 text-lg hover:underline hover:opacity-80">
+                                Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/about" className="transition-all duration-300 text-lg hover:underline hover:opacity-80">
+                                About
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/contact" className="transition-all duration-300 text-lg hover:underline hover:opacity-80">
+                                Contact
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+                <div className='flex items-center space-x-4'>
+                    <ModeToggle />
+                    <Button variant="outline" className="transition-all duration-300 hover:scale-105 hover:shadow-md">Sign In</Button>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
