df35ca2cde174d2efdd8fdd02ce918ff49064707
dipesh227 <dipeshjoshi227@gmail.com>
2024-09-29 16:48:16 +0530

    fix3

--- pages/api/set-cookie.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.setHeader("Set-Cookie", [
-    `myCookie=value; SameSite=Strict; Secure; HttpOnly; Path=/`,
-  ]);
-  res.status(200).json({ message: "Cookie set" });
-}
