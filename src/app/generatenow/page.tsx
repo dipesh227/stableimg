ff9f6e0407c7b8beb76440f8c9c36865f63088d9
dipesh227 <dipeshjoshi227@gmail.com>
2024-09-30 19:24:40 +0530

    image need

---
+++ src/app/generatenow/page.tsx
@@ -0,0 +1,8 @@
+export default function GenerateNowPage() {
+    return (
+        <div>
+            <h1>Generate Now</h1>
+            {/* Add content related to image generation here */}
+        </div>
+    );
+}
